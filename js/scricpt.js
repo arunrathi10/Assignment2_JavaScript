b063baa9f72eace0ff192d9dc3e3c5b10a76e43a
arunrathi10 <200587971@student.georgianc.on.ca>
2025-03-19 23:26:06 -0400

    added event listener on submit

--- js/scricpt.js
+++ js/scricpt.js
@@ -2,5 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const orderForm = document.getElementById("orderForm"),
           orderConfirmation = document.getElementById("orderConfirmation"),
           newOrderButton = document.getElementById("newOrderButton");
+
+          orderForm.addEventListener("submit", e => {
+            e.preventDefault();
+          });
+          
   });
   
