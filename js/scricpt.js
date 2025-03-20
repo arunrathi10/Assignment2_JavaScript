a9249080b8eac2c5cb3ebd41da34573a62abad27
tannulakra <tannulakra799@gmail.com>
2025-03-19 23:35:59 -0400

    getting value from input fields

--- js/scricpt.js
+++ js/scricpt.js
@@ -5,7 +5,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
           orderForm.addEventListener("submit", e => {
             e.preventDefault();
-          });
+
+          const get = id => document.getElementById(id).value.trim();
+          const firstName = get("firstName"),
+          lastName = get("lastName"),
+          address = get("address"),
+          phone = get("phone"),
+          size = document.getElementById("size").value,
+          sugars = document.getElementById("sugars").value,
+          glasses = parseInt(document.getElementById("glasses").value) || 1;
+
+        });
           
   });
   
