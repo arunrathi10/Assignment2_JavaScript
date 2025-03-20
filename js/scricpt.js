eb61b48e5c4b5c725382e9795af0013eb9416d7c
tannulakra <tannulakra799@gmail.com>
2025-03-19 23:51:03 -0400

    getting selected check boxes

--- js/scricpt.js
+++ js/scricpt.js
@@ -14,11 +14,20 @@ document.addEventListener("DOMContentLoaded", () => {
           size = document.getElementById("size").value,
           sugars = document.getElementById("sugars").value,
           glasses = parseInt(document.getElementById("glasses").value) || 1;
-          
+
           if (!firstName || !lastName || !address || !phone) {
             alert("Please fill out all required fields.");
             return;
           }
+          const fruits = [];
+          document.querySelectorAll('input[name="fruits"]:checked').forEach(input => {
+          fruits.push(input.value);
+          });
+
+          const toppings = [];
+          document.querySelectorAll('input[name="toppings"]:checked').forEach(input => {
+          toppings.push(input.value);
+        });
 
 
 
